7c1eeba03a88fa1bb63c069f8981d399ff33f149
rockycsumb <romoreno@csumb.edu>
2020-07-29 06:40:00 +0000

    changed port

--- server.js
+++ server.js
@@ -9,16 +9,14 @@ const app = express();
 
 // app.get('/', (req, res)=>res.send('API Running'));
 
-// Serve static assets in production
-// if(process.env.NODE_ENV === 'production'){
-//    //Set Static folder
-// 	app.use(express.static('client/build'));
-// 	app.get('*', (req, res)=>{
-// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// 	})
-// }
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+   //Set Static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res)=>{
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
