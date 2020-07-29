87126b71c1908c6463981c21e50c395dfd368617
rockycsumb <romoreno@csumb.edu>
2020-07-29 04:30:02 +0000

    configuring files for deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//before deploy to heroku notes
+// commented out app.get (below), and added //Serve static assets, also added const path = require('path)
+// added this to package.json in scripts> underneath dev, "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+
+
+// app.get('/', (req, res)=>res.send('API Running'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+   //Set Static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res)=>{
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
