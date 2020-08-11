a4d78e75063ae80ff3deab71f05cfaa76d472814
rockycsumb <romoreno@csumb.edu>
2020-08-11 05:38:41 +0000

    brushing up resume cards

--- client/src/components/layout/Resume.js
+++ client/src/components/layout/Resume.js
@@ -134,42 +134,6 @@ const Resume = () =>{
 					</div>
 				</div>
 			</div>
-			
-			
-			{/*
-			<div className="Resume-content-container">
-				<div className="Resume-content">
-					<div className="Resume-content-column1">
-						<ul>
-							<li>MERN FullStack</li>
-							<li>JS</li>
-							<li>C++</li>
-							<li>PHP</li>
-						</ul>
-					</div>
-					<hr />
-					
-					<div className="Resume-content-column2">
-						<ul>
-							<li>B.S. Computer Science</li>
-							<li>A.A. Natural Science</li>
-							<li>Location: San Jose</li>
-							<li>
-								<Link
-									className="btn btn-warning "
-									to={myResume}
-									target="_blank"	
-									download
-								>
-									Resume
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			*/}
-			
 		</div>
 	)
 }
