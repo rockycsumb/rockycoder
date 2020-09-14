5ae9cb6549a060ae02acb0d32b969f6cdb8b5429
rockycsumb <romoreno@csumb.edu>
2020-09-14 05:31:37 +0000

    added coderchat

--- client/src/components/layout/Projects.js
+++ client/src/components/layout/Projects.js
@@ -92,6 +92,86 @@ const Projects = () =>{
 				</div>
 			</div>
 			</Fragment>
+			<hr className="Projects-divider" />
+			
+			<Fragment>
+			<div className="Projects-title-description">
+				<div className="Projects-container-item-name">
+					<NavLink 
+						className="Projects-item-name-link"
+						to={{ pathname: "https://coderchat-c0a8c.web.app/" }} 
+						target="_blank"
+								>
+					CoderChat
+					</NavLink>
+				</div>
+				<div className="Projects-container-item-description">
+					CoderChat - whatsApp replica for coders
+				</div>
+			</div>
+			
+			<div className="Projects-thumbnails">
+				<div className="flip-card">
+					<div className="flip-card-inner">
+						<div className="flip-card-front">
+								<img className="Projects-image" 
+									src={require("../../assets/images/coderchat_login.png")} />
+						</div>
+						<div className="d-flex flex-column justify-content-center flip-card-back">
+							<div>
+								<h5>CoderChat</h5>
+							</div>
+							<div>
+								<Link class="btn btn-warning" 
+									to={{ pathname: "https://coderchat-c0a8c.web.app/" }} 
+									role="button" 
+									target="_blank"
+								>
+									Demo
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="flip-card">
+					<div className="flip-card-inner">
+						<div className="flip-card-front">
+								<img className="Projects-image" 
+									src={require("../../assets/images/coderchat_chat.png")} />
+						</div>
+						<div className="d-flex flex-column justify-content-center flip-card-back">
+							<div>
+								<h5>CoderChat</h5>
+							</div>
+							<div>
+								<Link class="btn btn-warning" 
+									to={{ pathname: "https://coderchat-c0a8c.web.app/" }} 
+									role="button" 
+									target="_blank"
+								>
+									Demo
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div className="Projects-tech">
+				<div className="text-left">
+					<p className="mb-0">
+						<span className="font-weight-bold Projects-tech-title">Technology Used: </span>
+						<span className="font-weight-light Projects-tech-info">React, Node.js, HTML, CSS, JavaScript, Firebase
+						</span>
+					</p>
+					<p>
+						<span className="font-weight-bold Projects-tech-title">API's: </span>
+						<span className="font-weight-light Projects-tech-info">Firebase Google authentication
+						</span>
+					</p>
+				</div>
+			</div>
+			</Fragment>
 			
 			<hr className="Projects-divider" />
 			
