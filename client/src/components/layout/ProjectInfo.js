8fd03742c6b1bc06b86154f43fb762e05e489e3f
rockycsumb <romoreno@csumb.edu>
2020-11-18 08:58:15 +0000

    fixed conflicts

--- client/src/components/layout/ProjectInfo.js
+++ client/src/components/layout/ProjectInfo.js
@@ -36,11 +36,8 @@ const ProjectInfo = ({
 					<div className="flip-card-inner">
 						<div className="flip-card-front">
 								<img className="Projects-image" 
-<<<<<<< HEAD
 									src={thumb1} />
-=======
-									src={require(`../../assets/images/${thumb1}`)} />
->>>>>>> a75f4862983e3ae70dd45081e05e432a3b205461
+			
 						</div>
 						<div className="d-flex flex-column justify-content-center flip-card-back">
 							<div>
@@ -62,11 +59,9 @@ const ProjectInfo = ({
 					<div className="flip-card-inner">
 						<div className="flip-card-front">
 								<img className="Projects-image" 
-<<<<<<< HEAD
+
 									src={thumb2} />
-=======
-									src={require(`../../assets/images/${thumb2}`)} />
->>>>>>> a75f4862983e3ae70dd45081e05e432a3b205461
+
 						</div>
 						<div className="d-flex flex-column justify-content-center flip-card-back">
 							<div>
--- client/src/components/layout/Projects.js
+++ client/src/components/layout/Projects.js
@@ -2,35 +2,12 @@ import React, {Fragment, useState, useEffect} from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import ProjectInfo from './ProjectInfo';
 import '../../assets/css/Projects.scss';
-import axios from 'axios';
+import {projectinfo} from '../../assets/projectinfo.js';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const Projects = () =>{
-
-	const [projectInfo, setProjectInfo] = useState({});
-
-	// useEffect(()=>{
-	// 	fetch('http://64.227.53.192/projects')
-	// 	.then(response => response.json())
-	// 	.then(data => {
-	// 		setProjectInfo(data);
-	// 	})
-	// }, [])
-
-	useEffect(()=>{
-		const res = async () =>{
-			await axios.get('https://curlyapi.com/wp-json/wp/v2/posts?_embed')
-			.then(data => {
-				setProjectInfo(data.data) 
-			})
-		}
-		res();
-	},[])
-	
-	console.log("setProjectInfo data ", projectInfo);
-
 	return(
 		<div className="d-flex flex-column">
-		<span id="delivery_data_url" data-url="{{ route('delivery-data') }}"></span>
 			<div className="Projects-title">
 				<div className="Projects-title-item1">
 					Projects
@@ -44,16 +21,17 @@ const Projects = () =>{
 				<Fragment>
 					<ScrollAnimation animateIn="fadeIn" duration={1.5}>
 					<ProjectInfo 
-						projectLiveLink={project.acf.project_livelink}
-						projectTitle={project.acf.project_title}
-						projectDescription={project.acf.project_description}
-						thumb1={project.acf.project_image_1.sizes.medium}
-						thumb2={project.acf.project_image_2.sizes.medium}
-						projectBackCardTitle={project.acf.project_backcardtitle}
-						demoUrl1={project.acf.project_demourl1}
-						demoUrl2={project.acf.project_demourl2}
-						techUsed={project.acf.project_techused}
-						apiUsed={project.acf.project_apiused}
+						projectLiveLink={project.projectLiveLink}
+						projectTitle={project.projectTitle}
+						projectDescription={project.projectDescription}
+						thumb1={project.thumb1}
+						thumb2={project.thumb2}
+						projectBackCardTitle={project.projectBackCardTitle}
+						demoUrl1={project.demoUrl1}
+						demoUrl2={project.demoUrl2}
+						techUsed={project.techUsed}
+						apiUsed={project.apiUsed}
+
 					/>
 					</ScrollAnimation>
 					
