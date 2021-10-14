5ee1124addef7528441bafe365d88792ae2afd4f
rockycsumb <romoreno@csumb.edu>
2021-10-14 06:38:17 -0700

    added curly admin demo link

--- client/src/assets/projectinfo.js
+++ client/src/assets/projectinfo.js
@@ -1,13 +1,13 @@
 export const projectinfo = [
 	{
-		projectLiveLink: "https://www.curlyadmin.com/demo/curlyadmin",
+		projectLiveLink: "https://curlyadmin-dev.herokuapp.com/demo/curlyadmin",
 		projectTitle: "CurlyAdmin.com",
 		projectDescription: "A virtual admin system",
 		thumb1: "curlyadmin_frontpage.png",
 		thumb2: "curlyadmin_dashboard.png",
 		projectBackCardTitle: "CurlyAdmin.com",
-		demoUrl1: "https://www.curlyadmin.com/demo/curlyadmin",
-		demoUrl2: "https://www.curlyadmin.com/demo/curlyadmin/login",
+		demoUrl1: "https://curlyadmin-dev.herokuapp.com/demo/curlyadmin",
+		demoUrl2: "https://curlyadmin-dev.herokuapp.com/demo/curlyadmin/Login",
 		techUsed: "Mongo DB, Express, React, Node.js (MERN Full Stack), Authentication Dashboard, CRUD, HTML, CSS, JavaScript",
 		apiUsed: "Sendgrid, Emailjs, Recaptcha, Stripe"
 	},
